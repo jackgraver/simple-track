7744b27b700a63bbb221fb36ea2f1a7e50c3f927
Jack.Graver <jackhgraver@gmail.com>
2025-09-25 10:13:25 -0600

    Initial components and api calls

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>
-    <!-- <NuxtRouteAnnouncer />
-    <NuxtWelcome />  -->
-    <NuxtPage />
-  </div>
+    <div>
+        <NuxtPage />
+    </div>
 </template>
+
+<style>
+html {
+    color: white;
+    background-color: black;
+}
+
+button {
+    background: #3a88c7;
+    padding: 6px 12px;
+    margin: 4px;
+    border-radius: 4px;
+    color: white;
+    border: none;
+}
+
+button:hover {
+    background: #4baeff;
+}
+</style>
---
+++ frontend/app/components/ExpectedMeals.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import type { MealPlanDay } from "~/types/models";
+import { computed } from "vue";
+
+const { data, pending, error } = useApiFetch<{
+    date: string;
+    days: MealPlanDay[];
+}>("mealplan/today");
+
+const expectedMeals = computed(() => {
+    if (!data.value?.days || data.value.days.length === 0) return [];
+    return (
+        data?.value?.days[0]?.meals?.filter(
+            (m: any) => m.status === "expected",
+        ) || []
+    );
+});
+</script>
+
+<template>
+    <div v-if="pending">Loading...</div>
+    <div v-else-if="error">Error: {{ error.message }}</div>
+    <div v-else>
+        <button v-for="meal in expectedMeals" :key="meal.ID">
+            {{ meal.meal.name }}
+        </button>
+    </div>
+</template>
